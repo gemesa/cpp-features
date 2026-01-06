8502c288b68c4aeb044508735e950c1a7d2584e3
Andras Gemes <gemesa@protonmail.com>
2026-01-06 22:08:30 +0100

    refactor: split out view in std::views

--- src/c++20/std-views.cpp
+++ src/c++20/std-views.cpp
@@ -6,9 +6,11 @@
 int main() {
   std::vector<uint64_t> addresses{0x1000, 0x1010, 0x2000, 0x1020, 0x3000};
 
-  for (auto addr :
-       addresses | std::views::filter([](auto a) { return a < 0x2000; }) |
-           std::views::transform([](auto a) { return a - 0x1000; })) {
+  auto view = addresses |
+              std::views::filter([](auto a) { return a < 0x2000; }) |
+              std::views::transform([](auto a) { return a - 0x1000; });
+
+  for (auto addr : view) {
     std::cout << "0x" << std::hex << addr << "\n";
   }
   return 0;

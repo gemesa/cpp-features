f64895539531cb378169d184b65a2e02eb76a834
Andras Gemes <gemesa@protonmail.com>
2026-01-12 15:07:21 +0100

    fix: rename log in std::bind_front to solve possible symbol conflicts

--- src/c++20/std-bind_front.cpp
+++ src/c++20/std-bind_front.cpp
@@ -2,14 +2,14 @@
 #include <functional>
 #include <iostream>
 
-void log(const char *level, uint64_t addr, const char *msg) {
+void logMsg(const char *level, uint64_t addr, const char *msg) {
   std::cout << "[" << level << "] 0x" << std::hex << addr << ": " << msg
             << "\n";
 }
 
 int main() {
-  auto logError = std::bind_front(log, "ERROR");
-  auto logInfo = std::bind_front(log, "INFO");
+  auto logError = std::bind_front(logMsg, "ERROR");
+  auto logInfo = std::bind_front(logMsg, "INFO");
 
   logError(0x10001000, "Invalid instruction.");
   logInfo(0x10001000, "Entry point.");
